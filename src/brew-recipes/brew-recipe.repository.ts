e09760abd462c5a4a415559198b0090b9bab4bfa
Alan Smillie <alan.smillie@gmail.com>
2021-03-09 09:46:59 -0500

    Create repository files for recipes and steps

---
+++ src/brew-recipes/brew-recipe.repository.ts
@@ -0,0 +1,28 @@
+import { BrewRecipeService } from "./brew-recipe.service";
+import { BrewRecipeItem } from "./models/BrewRecipeItem";
+import { CreateBrewRecipeRequest } from "./requests/CreateBrewRecipeRequest";
+import { UpdateBrewRecipeRequest } from "./requests/UpdateBrewRecipeRequest";
+
+export class BrewRecipeRepository {
+    private readonly brewRecipeService: BrewRecipeService;
+
+    constructor() {
+        this.brewRecipeService = new BrewRecipeService();
+    }
+
+    async getAllBrewRecipesByUserId(userId: string): Promise<BrewRecipeItem[]> {
+        return await this.brewRecipeService.getAllBrewRecipesByUserId(userId);
+    }
+
+    async createBrewRecipe(userId: string, createBrewRecipeRequest: CreateBrewRecipeRequest): Promise<BrewRecipeItem> {
+        return await this.createBrewRecipe(userId, createBrewRecipeRequest);
+    }
+
+    async updateBrewRecipe(userId: string, recipeId: string, updateBrewRecipeRequest: UpdateBrewRecipeRequest): Promise<BrewRecipeItem> {
+        return await this.updateBrewRecipe(userId, recipeId, updateBrewRecipeRequest);
+    }
+
+    async deleteBrewRecipeById(userId: string, recipeId: string): Promise<BrewRecipeItem> {
+        return await this.deleteBrewRecipeById(userId, recipeId);
+    }
+}
---
+++ src/brew-recipes/recipe-step.repository.ts
@@ -0,0 +1,36 @@
+import { RecipeStepItem } from "./models/RecipeStepItem";
+import { RecipeStepService } from "./recipe-step.service";
+import { CreateRecipeStepRequest } from "./requests/CreateRecipeStepRequest";
+import { UpdateRecipeStepRequest } from "./requests/UpdateRecipeStepRequest";
+
+export class RecipeStepRepository {
+    private readonly recipeStepService: RecipeStepService;
+    
+    constructor() {
+        this.recipeStepService = new RecipeStepService();
+    }
+
+    async getAllRecipeStepsByRecipeId(recipeId: string): Promise<RecipeStepItem[]> {
+        return await this.recipeStepService.getAllRecipeStepsByRecipeId(recipeId);
+    }
+
+    async createRecipeStep(createRecipeStepRequest: CreateRecipeStepRequest): Promise<RecipeStepItem> {
+        return await this.createRecipeStep(createRecipeStepRequest);
+    }
+
+    async updateRecipeStep(recipeId: string, stepId: string, updateRecipeStepRequest: UpdateRecipeStepRequest): Promise<RecipeStepItem> {
+        return await this.recipeStepService.updateRecipeStep(recipeId, stepId, updateRecipeStepRequest);
+    }
+
+    async deleteRecipeStepById(recipeId: string, stepId: string): Promise<RecipeStepItem> {
+        return await this.recipeStepService.deleteRecipeStepById(recipeId, stepId);
+    }
+
+    async saveThumbnailUrl(recipeId: string, stepId: string): Promise<void> {
+        return await this.recipeStepService.saveThumbnailUrl(recipeId, stepId);
+    }
+
+    getUploadUrl(stepId: string): string {
+        return this.recipeStepService.getUploadUrl(stepId);
+    }
+}
